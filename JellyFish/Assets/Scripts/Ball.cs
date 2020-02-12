b30f95acf2679453ecf9a6441fe9876aeca36f7d
amansavani <savaniaman@gmail.com>
2020-02-11 19:41:53 -0800

    shooting works

---
+++ JellyFish/Assets/Scripts/Ball.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Ball : MonoBehaviour {
+
+	public enum BALL_TYPE {
+		NONE = -1,
+		TYPE_1,
+		TYPE_2,
+		TYPE_3,
+		TYPE_4,
+		TYPE_5
+	}
+
+
+
+}
